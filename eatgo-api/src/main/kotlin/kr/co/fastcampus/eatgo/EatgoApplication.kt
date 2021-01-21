6005420f14b0c50b8b37effe20d9afe259822eda
Jack <gidools@gmail.com>
2021-01-21 22:30:53 +0900

    Initial commit

---
+++ eatgo-api/src/main/kotlin/kr/co/fastcampus/eatgo/EatgoApplication.kt
@@ -0,0 +1,11 @@
+package kr.co.fastcampus.eatgo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EatgoApplication
+
+fun main(args: Array<String>) {
+	runApplication<EatgoApplication>(*args)
+}
