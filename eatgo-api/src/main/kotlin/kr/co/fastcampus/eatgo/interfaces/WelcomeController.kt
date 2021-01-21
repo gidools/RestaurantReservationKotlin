5723af637561f1af76535df4cbf3730d5e89fc8f
Jack <gidools@gmail.com>
2021-01-21 22:38:43 +0900

    Add WelcomeController

---
+++ eatgo-api/src/main/kotlin/kr/co/fastcampus/eatgo/interfaces/WelcomeController.kt
@@ -0,0 +1,14 @@
+package kr.co.fastcampus.eatgo.interfaces
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class WelcomeController {
+
+    @GetMapping("/")
+    fun hello(): String {
+        return "Hello world"
+    }
+    
+}
