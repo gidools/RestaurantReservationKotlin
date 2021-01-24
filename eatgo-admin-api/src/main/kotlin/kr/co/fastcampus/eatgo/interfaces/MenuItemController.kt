e5d456b8900d1e200da8934a312948bcc86a8913
Jack <gidools@gmail.com>
2021-01-24 22:58:52 +0900

    Use file database

--- eatgo-admin-api/src/main/kotlin/kr/co/fastcampus/eatgo/interfaces/MenuItemController.kt
+++ eatgo-admin-api/src/main/kotlin/kr/co/fastcampus/eatgo/interfaces/MenuItemController.kt
@@ -3,10 +3,7 @@ package kr.co.fastcampus.eatgo.interfaces
 import kr.co.fastcampus.eatgo.application.MenuItemService
 import kr.co.fastcampus.eatgo.domain.MenuItem
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.PatchMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class MenuItemController {
@@ -18,6 +15,11 @@ class MenuItemController {
     fun bulkUpdate(@PathVariable("restaurantId") restaurantId: Long,
                    @RequestBody menuItems: List<MenuItem>): String {
         return menuItemService.bulkUpdate(restaurantId, menuItems)
+    }
+
+    @GetMapping(API_MENU_ITEMS)
+    fun list(@PathVariable("restaurantId") restaurantId: Long): List<MenuItem> {
+        return menuItemService.getMenuItems(restaurantId)
     }
 
     companion object {
--- eatgo-admin-api/src/test/kotlin/kr/co/fastcampus/eatgo/interfaces/MenuItemControllerTest.kt
+++ eatgo-admin-api/src/test/kotlin/kr/co/fastcampus/eatgo/interfaces/MenuItemControllerTest.kt
@@ -1,8 +1,9 @@
 package kr.co.fastcampus.eatgo.interfaces
 
+import com.nhaarman.mockito_kotlin.given
 import kr.co.fastcampus.eatgo.application.MenuItemService
-import org.hamcrest.MatcherAssert.assertThat
-import org.hamcrest.Matchers.`is`
+import kr.co.fastcampus.eatgo.domain.MenuItem
+import org.hamcrest.CoreMatchers.containsString
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.ArgumentMatchers.eq
@@ -14,7 +15,9 @@ import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @ExtendWith(SpringExtension::class)
@@ -29,12 +32,29 @@ class MenuItemControllerTest {
 
     @Test
     fun bulkUpdate() {
-        mvc.perform(MockMvcRequestBuilders.patch("/restaurants/1/menuitems")
+        mvc.perform(patch("/restaurants/1/menuitems")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content("[]"))
                 .andExpect(status().isOk)
 
         verify(menuItemService).bulkUpdate(eq(1L), anyList())
+    }
+
+    @Test
+    fun list() {
+        given(menuItemService.getMenuItems(1004L)).willReturn(
+            listOf(
+                MenuItem(name = "Kimchi")
+            )
+        )
+
+        mvc.perform(get("/restaurants/1004/menuitems")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content("[]"))
+            .andExpect(status().isOk)
+            .andExpect(content().string(containsString("Kimchi")))
+
+        verify(menuItemService).getMenuItems(1004L)
     }
 
 }
