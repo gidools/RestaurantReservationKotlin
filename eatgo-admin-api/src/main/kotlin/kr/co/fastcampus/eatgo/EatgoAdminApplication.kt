e7af24ee42848a9a9b8c0368a374b59b008336a7
Jack <gidools@gmail.com>
2021-02-03 01:06:54 +0900

    Fix admin application starting error

--- eatgo-admin-api/src/main/kotlin/kr/co/fastcampus/eatgo/EatgoAdminApplication.kt
+++ eatgo-admin-api/src/main/kotlin/kr/co/fastcampus/eatgo/EatgoAdminApplication.kt
@@ -1,10 +1,22 @@
 package kr.co.fastcampus.eatgo
 
+import kr.co.fastcampus.eatgo.utils.JwtUtil
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class EatgoAdminApplication
+class EatgoAdminApplication {
+
+	@Value(value = "\${jwt.secret}")
+	private lateinit var secret: String
+
+	@Bean
+	fun jwtUtil(): JwtUtil {
+		return JwtUtil(secret)
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<EatgoAdminApplication>(*args)
